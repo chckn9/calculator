9503db717d819f1d24d4ae6b0abe2d394a09e2a3
chckn9 <thomasyeoh9@gmail.com>
2022-12-29 13:49:52 -0800

    Create operate() function

--- javascript.js
+++ javascript.js
@@ -5,3 +5,8 @@ function subtract(a, b) { return a - b; }
 function multiply(a, b) { return a * b; }
 
 function divide(a, b) { return a / b; }
+
+// Takes in operator and 2 numbers, then calls respective function
+function operate(operator, a, b) {
+    return operator(a, b);
+}
