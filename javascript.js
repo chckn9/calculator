f7c26dfa5f612fc162396e34909f72b930d9c96a
chckn9 <thomasyeoh9@gmail.com>
2022-12-29 14:00:08 -0800

    Add html for calculator

--- javascript.js
+++ javascript.js
@@ -7,6 +7,4 @@ function multiply(a, b) { return a * b; }
 function divide(a, b) { return a / b; }
 
 // Takes in operator and 2 numbers, then calls respective function
-function operate(operator, a, b) {
-    return operator(a, b);
-}
+function operate(operator, a, b) { return operator(a, b); }
