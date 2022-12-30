577a8b4a129c9a10907f13a205ac6c8cdc13f62e
chckn9 <thomasyeoh9@gmail.com>
2022-12-30 01:15:09 -0800

    Fix calculator logic

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-let displayValue, storedNum, operator, operatorToggled;
+let displayValue = "", storedNum, operator, operatorToggled;
 
 function add(a, b) { return +a + +b; }
 
@@ -7,6 +7,48 @@ function subtract(a, b) { return +a - +b; }
 function multiply(a, b) { return +a * +b; }
 
 function divide(a, b) { return +a / +b; }
+
+// Add eventListener to all buttons
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (operatorToggled === true) {
+            operatorToggled = false;
+            clearDisplay();
+        }
+        populateDisplay(btn);
+    });
+});
+
+// When an operator is pressed, store display number and operator
+const operators = document.querySelectorAll('.operator');
+operators.forEach(op => {
+    op.addEventListener('click', () => {
+        const display = document.querySelector('.display');
+        
+        // First number
+        if (typeof storedNum === 'undefined') {
+            operator = op.id;
+            storedNum = displayValue;
+            operatorToggled = true;
+            console.log("number 1");
+        } else if (op.id === 'equal') {
+            storedNum = givenOperator(operator, storedNum, displayValue);
+            operatorToggled = true;
+            displayValue = storedNum;
+            display.textContent = displayValue;  
+            console.log("number 2");  
+        } else {
+            storedNum = givenOperator(operator, storedNum, displayValue);
+            operator = op.id;
+            operatorToggled = true;
+            displayValue = storedNum;
+            display.textContent = displayValue;  
+            console.log("number 3");  
+        }
+
+    });
+});
 
 // Takes in operator and 2 numbers, then calls respective function
 function givenOperator(operator, a, b) { 
@@ -46,53 +88,14 @@ function populateDisplay(btn) {
 function clearDisplay() {
     const display = document.querySelector('.display');
     display.replaceChildren();
+    displayValue = "";
 }
-
-// Add eventListener to all buttons
-const buttons = document.querySelectorAll('.btn');
-buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        if (operatorToggled === true) {
-            operatorToggled = false;
-            clearDisplay();
-        }
-        populateDisplay(btn);
-    });
-});
 
 // Add eventListener to clear button
 const clearButton = document.querySelector('#clear');
 clearButton.addEventListener('click', () => {
     clearDisplay();
-    displayValue = undefined;
     storedNum = undefined;
     operator = undefined;
     operatorToggled = undefined;
 })
-
-// When an operator is pressed, store display number and operator
-const operators = document.querySelectorAll('.operator');
-operators.forEach(op => {
-    op.addEventListener('click', () => {
-        const display = document.querySelector('.display');
-        
-        // First number
-        if (typeof storedNum === undefined) {
-            operator = op.id;
-            storedNum = displayValue;
-            operatorToggled = true;
-        } else if (op.id === "equal") {
-            storedNum = givenOperator(operator, storedNum, displayValue);
-            operatorToggled = true;
-            displayValue = storedNum;
-            display.textContent = displayValue;    
-        } else {
-            storedNum = givenOperator(operator, storedNum, displayValue);
-            operate = op.id;
-            operatorToggled = true;
-            displayValue = storedNum;
-            display.textContent = displayValue;    
-        }
-
-    });
-});
