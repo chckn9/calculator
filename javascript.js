71b7bf095b026c61dc055147aabf454cfd5dd6e8
chckn9 <thomasyeoh9@gmail.com>
2022-12-30 00:44:12 -0800

    INCOMPLETE (has bugs): Add logic for calculator

--- javascript.js
+++ javascript.js
@@ -1,26 +1,98 @@
-let displayValue = "";
+let displayValue, storedNum, operator, operatorToggled;
+
+function add(a, b) { return +a + +b; }
+
+function subtract(a, b) { return +a - +b; }
+
+function multiply(a, b) { return +a * +b; }
+
+function divide(a, b) { return +a / +b; }
+
+// Takes in operator and 2 numbers, then calls respective function
+function givenOperator(operator, a, b) { 
+    if (operator === "divide")
+        return divide(a, b);
+    if (operator === "multiply")
+        return multiply(a, b);
+    if (operator === "subtract")
+        return subtract(a, b);
+    if (operator === "add")
+        return add(a, b);
+}
 
 // Displays number when button is pressed
 function populateDisplay(btn) {
     const display = document.querySelector('.display');
-    display.insertAdjacentHTML('beforeend', btn.id);
-    displayValue += btn.id;
+
+    /*
+    TO FIX:
+    - Do not allow decimal to be first "digit"
+    - Show negative sign if display is empty
+    - No double negatives or decimals
+    */
+    if (!isNaN((+(btn.id))) || btn.id === "decimal") {
+        console.log(+(btn.id));
+        if (btn.id === "decimal") {
+            display.insertAdjacentHTML('beforeend', ".");
+            displayValue += ".";
+        } else {
+            display.insertAdjacentHTML('beforeend', btn.id);
+            displayValue += btn.id;
+        }
+    }
 }
 
+// Clears the display
+function clearDisplay() {
+    const display = document.querySelector('.display');
+    display.replaceChildren();
+}
+
+// Add eventListener to all buttons
 const buttons = document.querySelectorAll('.btn');
 buttons.forEach(btn => {
     btn.addEventListener('click', () => {
+        if (operatorToggled === true) {
+            operatorToggled = false;
+            clearDisplay();
+        }
         populateDisplay(btn);
-    })
+    });
+});
+
+// Add eventListener to clear button
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', () => {
+    clearDisplay();
+    displayValue = undefined;
+    storedNum = undefined;
+    operator = undefined;
+    operatorToggled = undefined;
 })
 
-function add(a, b) { return a + b; }
+// When an operator is pressed, store display number and operator
+const operators = document.querySelectorAll('.operator');
+operators.forEach(op => {
+    op.addEventListener('click', () => {
+        const display = document.querySelector('.display');
+        
+        // First number
+        if (typeof storedNum === undefined) {
+            operator = op.id;
+            storedNum = displayValue;
+            operatorToggled = true;
+        } else if (op.id === "equal") {
+            storedNum = givenOperator(operator, storedNum, displayValue);
+            operatorToggled = true;
+            displayValue = storedNum;
+            display.textContent = displayValue;    
+        } else {
+            storedNum = givenOperator(operator, storedNum, displayValue);
+            operate = op.id;
+            operatorToggled = true;
+            displayValue = storedNum;
+            display.textContent = displayValue;    
+        }
 
-function subtract(a, b) { return a - b; }
-
-function multiply(a, b) { return a * b; }
-
-function divide(a, b) { return a / b; }
-
-// Takes in operator and 2 numbers, then calls respective function
-function operate(operator, a, b) { return operator(a, b); }
+    });
+});
